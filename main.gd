de688a93e677144f32bbac0199269a82772b3f64
Claudio Caruzo <cgcaruzo@gmail.com>
2020-02-01 20:50:34 -0300

    cambio multiplicador

--- main.gd
+++ main.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var multiplicador = 3
+var multiplicador = 50
 
 
 # Called when the node enters the scene tree for the first time.
