de0bc15469e40d066e264c9addf47aa86d696d78
Claudio Caruzo <cgcaruzo@gmail.com>
2020-02-01 11:32:31 -0300

    mantras.json

--- Generamantra.gd
+++ Generamantra.gd
@@ -4,42 +4,21 @@ var Mantra = preload("res://Mantra.tscn")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var mantras = [
-{"texto": 'Om', "concentracion": 5, "tiempoExp": 7, "nivel" : 1 },
-{"texto": 'Om mani', "concentracion": 5, "tiempoExp": 7, "nivel" : 1 },
-{"texto": 'Shanti', "concentracion": 5, "tiempoExp": 7, "nivel" : 1 },
-{"texto": 'Har Har Har', "concentracion": 5, "tiempoExp": 7, "nivel" : 2 },
-{"texto": 'Sat Gure Name', "concentracion": 5, "tiempoExp": 7, "nivel" : 2 },
-{"texto": 'Ad Gure Name', "concentracion": 5, "tiempoExp": 7, "nivel" : 2},
-{"texto": 'Llugad Gure Name', "concentracion": 5, "tiempoExp": 7, "nivel" : 3 },
-{"texto": 'Namo Amitoufo', "concentracion": 5, "tiempoExp": 7, "nivel" : 3 },
-{"texto": 'Namo Amitabha Buda', "concentracion": 5, "tiempoExp": 7, "nivel" : 3 },
-{"texto": 'Om Hanumate Nama', "concentracion": 5, "tiempoExp": 7, "nivel" : 4 },
-{"texto": 'Siri Guru Ve Name', "concentracion": 5, "tiempoExp": 7, "nivel" : 4 },
-{"texto": 'Om Namah Shivaya', "concentracion": 5, "tiempoExp": 7, "nivel" : 4 },
-{"texto": 'Om mani padme hum om', "concentracion": 5, "tiempoExp": 7, "nivel" : 5 },
-{"texto": 'Ong Namo Guru Dev Namo ', "concentracion": 5, "tiempoExp": 7, "nivel" : 5 },
-{"texto": 'Om gam ganapataye namaha', "concentracion": 5, "tiempoExp": 7, "nivel" : 5 },
-{"texto": 'Sat Patim Dehi Parameshwara', "concentracion": 5, "tiempoExp": 7, "nivel" : 6 },
-{"texto": 'Om Gum Ganapatayei Namaha', "concentracion": 5, "tiempoExp": 7, "nivel" : 6 },
-{"texto": 'Ap Sahae Hoa Sache Da Sacha Doa', "concentracion": 5, "tiempoExp": 7, "nivel" : 6 },
-{"texto": 'Lokah Samastah Sukhino Bhavantu', "concentracion": 5, "tiempoExp": 7, "nivel" : 7 },
-{"texto": 'Om Muni Muni Mahamuni Sakyamuni Svaha', "concentracion": 5, "tiempoExp": 7, "nivel" : 7 },
-{"texto": 'Om Sri Ganeshaya Namaha Om Ganesha Om', "concentracion": 5, "tiempoExp": 7, "nivel" : 7 },
-{"texto": 'Namo Tassa Bhagavato Arahato Samma Sambuddhassa', "concentracion": 5, "tiempoExp": 7, "nivel" : 8 },
-{"texto": 'Gate Gate Paragate Parasamgate Bodhi Soha Prajna Paramita', "concentracion": 5, "tiempoExp": 7, "nivel" : 8 },
-{"texto": 'Sambuddhassa Lokah Samastah Ganapatayei Mahamuni Shanti', "concentracion": 5, "tiempoExp": 7, "nivel" : 8 }
-]
+var mantras = {}
 
 var mantraActual
-var idMantra = 0
+var idMantra = 1
+var file = File.new()
 onready var timer = get_node("Timer")
 onready var screen_size = get_viewport_rect().size
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	generar(mantras[idMantra])
+	file.open("res://resources/mantras.json", file.READ)
+	mantras = parse_json(file.get_as_text())
+	file.close()
+	generar(mantras[str(idMantra)][randi()%mantras[str(idMantra)].size()])
 
 func generar(mantra):
 	randomize()
@@ -58,5 +37,5 @@ func generar(mantra):
 
 
 func _on_Timer_timeout():
-	idMantra = idMantra + 1
-	generar(mantras[idMantra])
+	#idMantra = idMantra + 1
+	generar(mantras[str(idMantra)][randi()%mantras[str(idMantra)].size()])
